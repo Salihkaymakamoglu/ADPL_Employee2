94cb6ae42b405d04a3b35a87a5d530ccd8f06a09
samihabbaba <sami.software@hotmail.com>
2022-11-06 16:23:57 +0200

    office details

--- src/app/offices/offices-routing.module.ts
+++ src/app/offices/offices-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { OfficeDetailsComponent } from "./office-details/office-details.component";
 import { OfficesComponent } from "./offices.component";
 
 const routes: Routes = [
   // { path: "", pathMatch: "full", redirectTo: "" },
   { path: "", component: OfficesComponent },
+  { path: ":id", component: OfficeDetailsComponent  },
 ];
 
 @NgModule({
--- src/app/offices/offices.module.ts
+++ src/app/offices/offices.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { OfficesRoutingModule } from './offices-routing.module';
 import { OfficesComponent } from './offices.component';
 import {MatCardModule} from '@angular/material/card';
+import { OfficeDetailsComponent } from './office-details/office-details.component';
 
 
 
 @NgModule({
   declarations: [
-    OfficesComponent
+    OfficesComponent,
+    OfficeDetailsComponent
   ],
   imports: [
     CommonModule,
